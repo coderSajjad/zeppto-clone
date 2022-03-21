1344c6ef76ad0c5a955a73dbeb37c0e0aa40d339
Sajjad hossain <100340734+coderSajjad@users.noreply.github.com>
2022-03-21 07:46:14 -0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Toggle Mobile Menu
+const menuBtn = document.querySelector('.mobile_menu_icon')
+const nav = document.querySelector('.nav')
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav_active')
+    
+    if(menuBtn.classList.contains('icofont-navigation-menu')) {
+        menuBtn.classList.remove('icofont-navigation-menu')
+        menuBtn.classList.add('icofont-close')
+    }else{
+        menuBtn.classList.add('icofont-navigation-menu')
+        menuBtn.classList.remove('icofont-close')
+    }
+})
